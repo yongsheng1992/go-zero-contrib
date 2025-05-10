495a50a36a6a8c93dad27b5a6b30d93876ae97ec
zhangyongsheng <zhangyongsheng@hahabianli.com>
2025-05-10 11:54:32 +0800

    feat: add MustNacosSubscriberV1

--- configcenter/nacos/subscriber.go
+++ configcenter/nacos/subscriber.go
@@ -3,7 +3,9 @@ package nacos
 import (
 	"sync"
 
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
 	"github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 	"github.com/zeromicro/go-zero/core/configcenter/subscriber"
 )
@@ -53,7 +55,7 @@ func (s *nacosSubscriber) onChange(namespace, group, dataId, data string) {
 	}
 }
 
-func MustNacosSubscriber(nacosClient config_client.IConfigClient, namespace, dataId, group string) subscriber.Subscriber {
+func MustNacosSubscriber(nacosClient config_client.IConfigClient, namespace, group, dataId string) subscriber.Subscriber {
 	configContent, err := nacosClient.GetConfig(vo.ConfigParam{DataId: dataId, Group: group})
 	if err != nil {
 		panic(err)
@@ -73,4 +75,15 @@ func MustNacosSubscriber(nacosClient config_client.IConfigClient, namespace, dat
 	}
 
 	return nacosSubscriber
+}
+
+func MustNacosSubscriberV1(serverConfig []constant.ServerConfig, clientConfig constant.ClientConfig, namespace, group, dataId string) subscriber.Subscriber {
+	nacosClient, err := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": serverConfig,
+		"clientConfig":  clientConfig,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return MustNacosSubscriber(nacosClient, namespace, group, dataId)
 }
